4969a84967b4a1fe8467d73444a9ca39cd7b838e
regardue <147426303+regardue@users.noreply.github.com>
2023-12-11 21:27:03 +0200

    s

---
+++ curs day-by-day/curs decembrie/11.12.2023.js
@@ -0,0 +1,110 @@
+// console.log("Hello!");
+
+
+// console.log("Text aici!!!!");
+// console.log("Hondru Daniel Ciprian");
+// alert("Alerta!");
+
+// document.getElementById('output').innerText='Am setat niste text';
+// let output=document.getElementById("output");
+// output.InnerText="Text aici!";
+// output.style.color="Red";
+
+// let text;
+// console.log(text);
+// text='vhbfnjdaghdgbad';
+// console.log(text);
+
+// String aka text
+
+// let text = "bhdagnadjg";
+
+// Number aka numar
+
+// let number = 100;
+
+// Boolean aka true/false
+
+// let boolean = true;
+// boolean =false; 
+
+// let a = 5;
+// let b = 6;
+// let c;
+// c = a-b; a/b a+b ab a%b(restul - par sau impar) a**b(a la puterea b)
+// let d = "mdkagmkmakmgkm";
+// let x = true;
+// a=a+1 = a++
+// a=a-1 = a--
+// a=a+120 = a+=120;
+// a*=10;
+// a/=10;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(a+b);
+// console.log(c);
+// console.log("Text:"+c+a+b);
+// console.log(`Text : ${c} si ${a} si ${b}`)
+// console.log("Text"+c+" si"+a+" si"+b)
+// let test= typeof(x);
+// console.log(test);
+
+
+// for(let i=0; i<100; i++)
+// {
+//     console.log(i);
+// }
+
+
+// let a=5;
+// // let b="4";
+// let b=parseInt('4');
+// console.log(a+b); // il transforma pe a in string
+// let c=parseInt('3xyz') //cauta primul numar din string si il adauga la numarul tau (a)
+// console.log(a+c)
+
+
+// let a=5;
+// a=a.toString();
+// let b=4;
+// console.log(a+b)
+
+
+// let age=11;
+// console.log(age);
+// console.log(age+=1);
+// console.log(age);
+// let name="Ciprian";
+// console.log(name);
+// let Print = typeof(name);
+// console.log(Print); // console.log(typeof(name));
+// console.log(age*=2);
+// console.log(age/=3);
+
+// let valoare=prompt(); // apare un box cu mesaj in care trebuie sa bagi ceva
+// console.log(valoare;)
+
+
+
+// function printName()
+// {
+//     //Aici scriem cod
+//     console.log('My Function!');
+//     let userInput=document.getElementById("name_input").value;
+//     console.log(userInput);
+// }
+// let suma=sum(5,4);
+// console.log(suma);
+
+// function sum(a,b)
+// {
+//     return a+b; //orice scrii dupa return nu se mai executa, daca se afla in interiorul functiei
+// }
+// MakeRed("green");
+
+// function MakeRed(color)
+// {
+//     document.getElementById("btn_submit").style.color=color;
+// }
