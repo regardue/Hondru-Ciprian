0a0a4058649363e2df3739d57bba6707b94cee20
regardue <147426303+regardue@users.noreply.github.com>
2023-12-20 23:37:33 +0200

    s

---
+++ curs day-by-day/curs decembrie/20.12.2023.js
@@ -0,0 +1,11 @@
+// function Hello(){
+    // let result = "";
+    // for(let i=0; i<=10; i++){
+    //     for(let j=0; i<=10; j++){
+    //         result += i*j + ",";
+    //     }
+    //     result += "\n";
+    // }
+
+    // document.body.innerText = result;
+// }   
