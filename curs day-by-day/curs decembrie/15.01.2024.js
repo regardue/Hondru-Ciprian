8747b278274252b812ff2b78995bd97210416896
regardue <147426303+regardue@users.noreply.github.com>
2024-01-16 00:19:37 +0200

    s

---
+++ curs day-by-day/curs decembrie/15.01.2024.js
@@ -0,0 +1,58 @@
+// TEMA ARRAY
+
+// let arr = [4,6,10,12]
+// console.log(arr[1]);
+
+// let arr = [4,20,30,10]
+// console.log(arr[arr.length-1])
+
+// let arr = [4,20,30,10,20];
+// let middle = Math.floor(arr.length/2);
+// console.log(arr[middle])
+
+// let arr = [23,22,30,11]
+// if(arr[0]==arr[arr.length-1]){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// arr = [23,5,30,11,12];
+// arr.sort((a,b)=>{return a-b});
+// console.log(arr[0]);
+
+// function addNumbers(){
+// let arr=[];
+// for(let i=0;i<5;i++){
+//     let i = parseInt(prompt('add 5 numbers'));
+//     arr.push(i);
+// }
+// arr.sort((a,b)=>b-a);
+// let maxNumber = 0; 
+// maxNumber=arr[0];
+// document.getElementById("max").innerText = `max number is:${maxNumber}`;
+// }
+
+// let max_number=0;
+// for(i=0;i<10;i++){
+//     let numarCurent = Math.floor(Math.random()*1000+1);
+//     if (max_number<numarCurent){
+//         max_number=numarCurent;
+//     }
+// }
+// console.log(max_number)
+
+// function addNumbers(){
+//     let arr = [];
+//     for(i=0;i<5;i++){
+//         let input=prompt("enter a number");
+//         let number=parseFloat(input);
+//         arr.push(number);
+//     }
+//     console.log(arr);
+//     let maxNr=Math.max(...arr);
+//     document.getElementById("max").innerText = `max nr ${maxNR}`
+// }
+
+
