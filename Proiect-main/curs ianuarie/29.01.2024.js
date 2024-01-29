9f49c57887c0ae886de14ba675fd284f629e4c70
regardue <147426303+regardue@users.noreply.github.com>
2024-01-29 19:02:36 +0200

    s

--- Proiect-main/curs ianuarie/29.01.2024.js
+++ Proiect-main/curs ianuarie/29.01.2024.js
@@ -1,8 +1,50 @@
-let car = new Object();
-car.make=`Mazda`;
-car.year=2024;
-car.color=`red`;
+// let car = new Object();
+// car.make=`Mazda`;
+// car.year=2024;
+// car.color=`red`;
 
-console.log(car.make);
+// console.log(car.make);
 
-let
+// let car ={
+//     make:`Mazda`
+//     year:2024
+//     color:'red'
+// }
+
+
+// console.log(car);
+
+// console.log(car.make);
+// console.log(car['year']);
+
+
+// let keys=Object.keys(car);
+// let values = Object.entries(car);
+// console.log(keys);
+// console.log(values);
+
+
+//constructor
+function Car(make,year,color){
+    this.make=make;
+    this.year=year;
+    this.color=color;
+    this.printInfo=()=>{
+        console.log(`${make},${year},${color}`);
+    }
+    this.addYear=()=>{
+        this.year++;
+        return this.year;
+    }
+}
+let mazda = new Car(`Mazda`,2024,"red");
+let dacia = new Car("Dacia",1900,"white");
+console.log(mazda); //accesam obiectul mazda
+console.log(dacia);
+mazda.addYear();
+mazda.addYear();
+mazda.addYear();
+mazda.addYear();
+mazda.printInfo();
+
+
