783161382d1f9ad033a62a76ceb32f77ada097a4
regardue <147426303+regardue@users.noreply.github.com>
2024-02-21 20:13:44 +0200

    s

--- curs day-by-day/curs februarie/21.02.2024.js
+++ curs day-by-day/curs februarie/21.02.2024.js
@@ -14,7 +14,6 @@
 // let arr4 = arr.filter(x => x%3==0);
 // console.log(arr4);
 
-
 // let arr5 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
 // let result = arr5.filter(isPrime);
 // function isPrime(x){
@@ -30,6 +29,33 @@
 
 // reduce
 
-let array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
-let result = array.reduce((a,b) => a+b);
-console.log(result);
+// let array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+// let result = array.reduce((a,b) => a+b);
+// console.log(result);
+
+// let getMax = (a,b) => Math.max(a,b)
+// let result2 = array.reduce(getMax,5)
+// console.log(result2);
+
+// let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// let result = array.find(x => x>7)
+// console.log(result)
+
+// let arr = [{name:"cris"},{name:"cris2"}]
+// let name = arr.findIndex(x => x.name == "cris2") // findIndex intoarce pozitia
+// console.log(name)
+
+// some
+
+// let some=[0,1,2,3,4,5,6,7,8,9,10,11,12,13].some(x => x>4)
+// console.log(some)
+
+// let arr=[{name:"andrei"},{name:"calin"}].some(x=>x.name == "calin")
+// console.log(arr);
+
+// every
+
+// let array=[0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+// let every = array.every(x => x>=0);
+// console.log(every);
+
