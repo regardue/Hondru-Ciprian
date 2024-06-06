f887606e7c7d5920569009315daa0d5a7b4d1c68
regardue <147426303+regardue@users.noreply.github.com>
2024-06-06 20:46:38 +0300

    s

---
+++ react-by-horia/vite-project/components/auth/Auth.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import Register from "../register/Register";
+
+function Auth() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Auth;
---
+++ react-by-horia/vite-project/components/register/Register.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+function Register(props) {
+  return (
+    <>
+      <button></button>
+    </>
+  )
+}
+
+export default Register;
