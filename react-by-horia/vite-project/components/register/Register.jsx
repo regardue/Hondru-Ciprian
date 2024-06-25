c6cc367a35ebcf717bf97f98b0d825071d7b49b2
regardue <147426303+regardue@users.noreply.github.com>
2024-06-25 21:19:29 +0300

    s

--- react-by-horia/vite-project/components/register/Register.jsx
+++ react-by-horia/vite-project/components/register/Register.jsx
@@ -1,72 +1,200 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { validationRules } from "../../../utils/validations/validation";
 
 function Register() {
+	const [form, setForm] = useState({
+		email: "",
+		firstName: "",
+		lastName: "",
+		age: "",
+		password: "",
+		confirmPassword: "",
+	});
+	const [errors, setErrors] = useState({
+		email: null,
+		firstName: null,
+		lastName: null,
+		age: null,
+		password: null,
+		confirmPassword: null,
+	});
+	const [isFormValid, setIsFormValid] = useState(false);
 
-    const[form, setForm] = useState({email:'',password:''});
-    const[errors,setErrors] = useState({email:'',password:''});
-    const[isFormValid, setIsFormValid] = useState(false);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		// console.log(name);
+		// console.log(value);
+		setForm({ ...form, [name]: value });
+		validateFields(name, value);
+	};
 
-    const handleChange = (e) =>{
-        const {name,value} = e.target;
-        console.log(name);
-        console.log(value);
-        setForm({...form,[name]:value});
-        validateFields(name,value);
-    };
+	const validateFields = (fieldName, inputValue) => {
+		let newErrors = { ...errors };
+		const validationResponse = validationRules[fieldName](inputValue);
+		console.log("Validation Response");
+		console.log(validationResponse);
 
-    const validateFields = (fieldName, inputValue) => {
-        let newErrors = {...errors};
+		const NAMES_PATTERN = new RegExp(/^[^\d\s]+$/);
+		// const EMAIL_REGEX = new RegExp(/\S+@\S+\.\S+/);
 
-        const emailRegex = /\S+@\S+\.\S+/;
-        if(fieldName === 'email'){
-            if(!inputValue){
-                newErrors.email = 'Email is required.'
-                setIsFormValid(false);
-            }
-            else if(!emailRegex.test(inputValue)){
-                newErrors.email = 'Email is in the wrong format.'
-                setIsFormValid(false);
-            }
-            else{
-                newErrors.email = "";
-                setIsFormValid(true);
-            }
-        }
-        if(fieldName === 'password'){
-            if(!inputValue){
-                newErrors.password = 'Password is required.'
-                setIsFormValid(false);
-            }
-            else{
-                newErrors.password = "";
-                setIsFormValid(true);
-            }
-        }
-        setErrors(newErrors);
-    }
+		// if (fieldName === "email") {
+		// 	if (!inputValue) {
+		// 		newErrors.email = "Email is required";
+		// 		setIsFormValid(false);
+		// 	} else if (!EMAIL_REGEX.test(inputValue)) {
+		// 		newErrors.email = "Email is in the wrong format";
+		// 		setIsFormValid(false);
+		// 	} else {
+		// 		newErrors.email = "";
+		// 		// setIsFormValid(true);
+		// 	}
+		// }
+		if (fieldName === "firstName") {
+			if (!inputValue) {
+				newErrors.firstName = "First Name is required";
+				setIsFormValid(false);
+			} else if (inputValue.length < 2) {
+				newErrors.firstName = "First Name must have at least 2 charachters";
+				setIsFormValid(false);
+			} else if (!NAMES_PATTERN.test(inputValue)) {
+				newErrors.firstName = "First name can't contain numbers";
+				setIsFormValid(false);
+			} else {
+				newErrors.firstName = "";
+				// setIsFormValid(true);
+			}
+		}
+		if (fieldName === "lastName") {
+			if (!inputValue) {
+				newErrors.lastName = "Last Name is required";
+				setIsFormValid(false);
+			} else if (inputValue.length < 2) {
+				newErrors.lastName = "Last Name must have at least 2 charachters";
+				setIsFormValid(false);
+			} else if (!NAMES_PATTERN.test(inputValue)) {
+				newErrors.lastName = "Last name can't contain numbers";
+				setIsFormValid(false);
+			} else {
+				newErrors.lastName = "";
+				// setIsFormValid(true);
+			}
+		}
+		if (fieldName === "age") {
+			if (!inputValue) {
+				newErrors.age = "Age is required";
+				setIsFormValid(false);
+			} else if (isNaN(inputValue)) {
+				newErrors.age = "Age must be a number!";
+				setIsFormValid(false);
+			} else if (inputValue < 18) {
+				newErrors.age = "Must be older than 18 years old!";
+				setIsFormValid(false);
+			} else if (inputValue > 100) {
+				newErrors.age = "Must be younger than 100 years old!";
+				setIsFormValid(false);
+			} else {
+				newErrors.age = "";
+				// setIsFormValid(true);
+			}
+		}
+		if (fieldName === "password") {
+			if (!inputValue) {
+				newErrors.password = "Password is required";
+				setIsFormValid(false);
+			} else {
+				newErrors.password = "";
+				// setIsFormValid(true);
+			}
+		}
+		if (fieldName === "confirmPassword") {
+			if (!inputValue) {
+				newErrors.confirmPassword = "Confirm Password is required";
+				setIsFormValid(false);
+			} else if (inputValue !== form.password) {
+				newErrors.confirmPassword = "Passwords do not match";
+				setIsFormValid(false);
+			} else {
+				newErrors.confirmPassword = "";
+				// setIsFormValid(true);
+			}
+		}
+		const errorsArray = Object.values(newErrors);
+		const isValid = errorsArray.every((value) => value === "");
+		if (isValid) setIsFormValid(true);
+		setErrors(newErrors);
+	};
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log("Submitted form");
-        console.log(Object.entries(form).every((field) => field));
-    };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!isFormValid) return;
+		console.log("Submitted form");
+	};
 
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="login_input">
+				<label>Email</label>
+				<input
+					type="email"
+					name="email"
+					value={form.email}
+					onChange={handleChange}
+				/>
+				<span>{errors.email && <p>{errors.email}</p>}</span>
+			</div>
+			<div className="login_input">
+				<label>First Name</label>
+				<input
+					type="text"
+					name="firstName"
+					value={form.firstName}
+					onChange={handleChange}
+				/>
+				<span>{errors.firstName && <p>{errors.firstName}</p>}</span>
+			</div>
+			<div className="login_input">
+				<label>Last Name</label>
+				<input
+					type="text"
+					name="lastName"
+					value={form.lastName}
+					onChange={handleChange}
+				/>
+				<span>{errors.lastName && <p>{errors.lastName}</p>}</span>
+			</div>
+			<div className="login_input">
+				<label>Age</label>
+				<input
+					type="text"
+					name="age"
+					value={form.age}
+					onChange={handleChange}
+				/>
+				<span>{errors.age && <p>{errors.age}</p>}</span>
+			</div>
+			<div className="login_input">
+				<label>Password</label>
+				<input
+					type="password"
+					name="password"
+					value={form.password}
+					onChange={handleChange}
+				/>
+				<span>{errors.password && errors.password}</span>
+			</div>
+			<div className="login_input">
+				<label>Confirm Password</label>
+				<input
+					type="password"
+					name="confirmPassword"
+					value={form.confirmPassword}
+					onChange={handleChange}
+				/>
+				<span>{errors.confirmPassword && <p>{errors.confirmPassword}</p>}</span>
+			</div>
+			<button type="submit">Sign up</button>
+		</form>
+	);
+}
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="login_input">
-          <label>Email</label>
-          <input type="email" name="email" value={form.email} onChange={handleChange} required></input>
-          <span>{errors.email && <p>{errors.email}</p>}</span>
-        </div>
-        <div className="login_input">
-          <label>Password</label>
-          <input type="password" name="password" value={form.password} onChange={handleChange} required></input>
-          <span>{errors.password && <p>{errors.password}</p>}</span>
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-  
-  export default Register;
+export default Register;
--- react-by-horia/vite-project/utils/validations/validation.js
+++ react-by-horia/vite-project/utils/validations/validation.js
@@ -1,14 +1,16 @@
-const NAMES_PATTERN = new RegExp(/^[^\d\s]+$/);
-const EMAIL_REGEX = new RegExp
+const NAMES_REGEX = new RegExp(/^[^\d\s]+$/);
+const EMAIL_REGEX = new RegExp(/\S+@\S+\.\S+/);
 const PASSWORD_REGEX = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
 const MIN_AGE = 18;
 const MAX_AGE = 100;
 
+let validationResponse = {
+    success:null,
+    message:null
+}
+
 const validateEmail = (inputValue) => {
-    let validationResponse = {
-        succes:null,
-        message:null
-    }
+    
     if(!inputValue){
         validationResponse.message = "Email is required"
         validationResponse.success = false;
@@ -21,7 +23,92 @@ const validateEmail = (inputValue) => {
         validationResponse.message = ""
         validationResponse.success = true;
     }
+    return validationResponse;
+}
+
+const validateName = (inputValue) => {
+    
+    if(!inputValue){
+        validationResponse.message = "Input is required"
+        validationResponse.success = false;
+    }
+    else if(!NAMES_REGEX.test(inputValue)){
+        validationResponse.message = "Input is in the wrong format"
+        validationResponse.success = false;
+    }
+    else if(inputValue.length<2){
+        validationResponse.message = "Input must have at least 2 characters";
+        validationResponse.success = false;
+    }
+    else{
+        validationResponse.message = ""
+        validationResponse.success = true;
+    }
+    return validationResponse;
+}
+
+const validateAge = (inputValue) => {
+    if(!inputValue){
+        validationResponse.message = "Age is required"
+        validationResponse.success = false;
+    }
+    else if(isNaN(inputValue)){
+        validationResponse.message = "Age must be a number"
+        validationResponse.success = false;
+    }
+    else if(inputValue.length<MIN_AGE){
+        validationResponse.message = "Must be older than 18";
+        validationResponse.success = false;
+    }
+    else if(inputValue.length>MAX_AGE){
+        validationResponse.message = "Must be younger than 100";
+        validationResponse.success = false;
+    }
+    else{
+        validationResponse.message = ""
+        validationResponse.success = true;
+    }
+    return validationResponse;
+}
+
+const validatePassword = (inputValue) => {
+    if(!inputValue){
+        validationResponse.message = "Password is required"
+        validationResponse.success = false;
+    }
+    else if(PASSWORD_REGEX.test(inputValue)){
+        validationResponse.message = "Password is in the wrong format"
+        validationResponse.success = false;
+    }
+    else{
+        validationResponse.message = ""
+        validationResponse.success = true;
+    }
+    return validationResponse;
+}
+
+const validateConfirmPassword = (inputValue) => {
+    if(!inputValue){
+        validationResponse.message = "Password is required"
+        validationResponse.success = false;
+    }
+    else if(passwordValue !== ){
+        validationResponse.message = "Passwords do not match"
+        validationResponse.success = false;
+    }
+    else{
+        validationResponse.message = ""
+        validationResponse.success = true;
+    }
+    return validationResponse;
 }
 
 
-export const validationRules;
+export const validationRules = {
+    email: validateEmail,
+    firstName: validateName,
+    lastName: validateName,
+    age: validateAge,
+    password: validatePassword,
+    confirmPassword: validateConfirmPassword
+}
