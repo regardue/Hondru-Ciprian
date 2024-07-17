3b207cee5c3ca87bc44e68614c7c5b9013404d2a
regardue <147426303+regardue@users.noreply.github.com>
2024-07-17 21:41:50 +0300

    s

---
+++ Firebase/Firebase2/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import BasicButtons from './components/button'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <BasicButtons/>
+    </>
+  )
+}
+
+export default App
